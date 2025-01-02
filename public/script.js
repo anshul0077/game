6e6f417a3bad1a6360fb30be278364255ad28701
Anshul <choudharyanshul220@gmail.com>
2025-01-02 19:39:18 +0530

    footer funcitonality completed

--- public/script.js
+++ public/script.js
@@ -4,9 +4,40 @@ let cpuCounter=document.querySelector("#CPU-counter")
 let rulesBtn=document.querySelector("#Rulesbtn")
 let resetBtn=document.querySelector("#Resetbtn")
 let rulesDiv=document.querySelector(".rules")
+let nav=document.querySelector(".nav")
 //footer ref
-rulesBtn.addEventListener('click',()=>{
-    rulesDiv.style.display="block"
-})
+let main=document.querySelector(".main")
+
+//main
+function rulesdivfxn(){
+    let flag=0
+    rulesBtn.addEventListener('click',()=>{
+        if(flag==0){
+        rulesDiv.style.display="block"
+        nav.style.filter="blur(10px)"
+        main.style.filter="blur(10px)"
+    flag=1
+       }
+    else{
+        rulesDiv.style.display="none"
+        nav.style.filter="blur(0px)"
+        main.style.filter="blur(0px)"
+flag=0
+    }
+    })
+    
+}
+rulesdivfxn()
+resetBtn.addEventListener('click',resetscore)
+function resetscore(){
+
+userCounter.innerHTML=0
+cpuCounter.innerHTML=0
+}
+
+//footer func completed
+
+
+
 
 
