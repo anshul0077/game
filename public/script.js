960d510228268201dd16e721e496b8f2d2c7e483
Anshul <choudharyanshul220@gmail.com>
2025-01-02 19:16:16 +0530

    adding functionally

---
+++ public/script.js
@@ -0,0 +1,12 @@
+let  userCounter=document.querySelector("#user-counter")
+let cpuCounter=document.querySelector("#CPU-counter")
+// nav ref
+let rulesBtn=document.querySelector("#Rulesbtn")
+let resetBtn=document.querySelector("#Resetbtn")
+let rulesDiv=document.querySelector(".rules")
+//footer ref
+rulesBtn.addEventListener('click',()=>{
+    rulesDiv.style.display="block"
+})
+
+
