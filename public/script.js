664b99dc17e9592ffafaae1d659c85d8f14f1d37
Anshul <choudharyanshul220@gmail.com>
2025-01-02 22:05:00 +0530

    mapping of user pick

--- public/script.js
+++ public/script.js
@@ -1,50 +1,98 @@
-let  userCounter=document.querySelector("#user-counter")
-let cpuCounter=document.querySelector("#CPU-counter")
-let nav=document.querySelector(".nav")
-// nav ref
-let rulesBtn=document.querySelector("#Rulesbtn")
-let resetBtn=document.querySelector("#Resetbtn")
-let rulesDiv=document.querySelector(".rules")
-const newLocal = ""
-let resetmsg=document.querySelector(".resetmsg")
-//footer ref
-let main=document.querySelector(".main")
+let userpick = document.querySelector("#user-Picked")
+let cpuimg = document.querySelector(".cpuimg")
+let images = [
+    { id: 0, iname: "scissor", src: "https://raw.githubusercontent.com/markteekman/rock-paper-scissors-game/58944aa93df3f58bfe876369f0576b4e025ae77e/public/icon-scissors.svg" },
+    { id: 1, iname: "rock", src: "https://raw.githubusercontent.com/markteekman/rock-paper-scissors-game/58944aa93df3f58bfe876369f0576b4e025ae77e/public/icon-rock.svg" },
+    { id: 2, iname: "paper", src: "https://raw.githubusercontent.com/markteekman/rock-paper-scissors-game/58944aa93df3f58bfe876369f0576b4e025ae77e/public/icon-paper.svg" },
+]
 
-//main
-function rulesdivfxn(){
-    let flag=0
-    rulesBtn.addEventListener('click',()=>{
-        if(flag==0){
-        rulesDiv.style.display="block"
-        nav.style.filter="blur(10px)"
-        main.style.filter="blur(10px)"
-    flag=1
-       }
-    else{
-        rulesDiv.style.display="none"
-        nav.style.filter="blur(0px)"
-        main.style.filter="blur(0px)"
-flag=0
+
+
+let uid = document.querySelector(".user-image-div")
+uid.addEventListener('click', (e) => {
+    let randumNum = Math.floor(Math.random() * 3)
+    console.log(randumNum);
+    if (e.target.parentElement.tagName === "BUTTON") {
+
+        console.log(e.target.parentElement.getAttribute("id"))
+        console.log("userchooses =");
+        randomImgae(randumNum)
+        userpick.style.display="flex"
+        
+
     }
+
+})
+function randomImgae(randumNum) {
+    if (randumNum == 0) {
+        console.log(images[0].iname)
+        cpuimg.src = images[0].src
+    }
+    else if (randumNum == 1) {
+        console.log(images[1].iname)
+        cpuimg.src = images[1].src
+    }
+    else if (randumNum == 2) {
+        console.log(images[2].iname)
+        cpuimg.src = images[2].src
+
+    }
+
+}
+
+
+
+
+//assets
+let userCounter = document.querySelector("#user-counter")
+let cpuCounter = document.querySelector("#CPU-counter")
+let nav = document.querySelector(".nav")
+// nav ref
+let rulesBtn = document.querySelector("#Rulesbtn")
+let resetBtn = document.querySelector("#Resetbtn")
+let rulesDiv = document.querySelector(".rules")
+
+let resetmsg = document.querySelector(".resetmsg")
+//footer ref
+let main = document.querySelector(".main")
+//main
+function rulesdivfxn() {
+    let flag = 0
+    rulesBtn.addEventListener('click', () => {
+        if (flag == 0) {
+            rulesDiv.style.display = "block"
+            nav.style.filter = "blur(10px)"
+            main.style.filter = "blur(10px)"
+            flag = 1
+        }
+        else {
+            rulesDiv.style.display = "none"
+            nav.style.filter = "blur(0px)"
+            main.style.filter = "blur(0px)"
+            flag = 0
+        }
     })
-    
+
 }
 rulesdivfxn()
-resetBtn.addEventListener('click',resetscore)
-function resetscore(){
-setTimeout(()=>{
-resetmsg.style.display='block'
+resetBtn.addEventListener('click', resetscore)
+function resetscore() {
+    setTimeout(() => {
+        resetmsg.style.display = 'block'
 
-},100)
-setTimeout(()=>{
-    resetmsg.style.display='none'
+    }, 100)
+    setTimeout(() => {
+        resetmsg.style.display = 'none'
 
-},2000)
-userCounter.innerHTML=0
-cpuCounter.innerHTML=0
+    }, 2000)
+    userCounter.innerHTML = 0
+    cpuCounter.innerHTML = 0
 }
 
 //footer func completed
+
+
+
 
 
 
